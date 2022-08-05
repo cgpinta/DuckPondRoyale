49e738c9e4f45e49e829d4ed67b045f485f91dae
Pintobean711 <36062747+Pintobean711@users.noreply.github.com>
2022-08-04 21:12:54 -0400

    Optimized Player loading script. Add countdown to beginning of fight

--- Assets/Scripts/Photon/PlayerSpawner.cs
+++ Assets/Scripts/Photon/PlayerSpawner.cs
@@ -4,96 +4,108 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
+using System;
+using Random = UnityEngine.Random;
 
 public class PlayerSpawner : MonoBehaviour
 {
     public CharacterList chars;
 
     [Header("Stage UI")]
-    public TMP_Text countdownText;
+    public TMP_Text countdownTextBox;
 
 
+    Timer countdownTimer = new Timer(true);
+    Animator countdownAnimator;
+    List<string> countdownText = new List<string>();
+    int countdownNextNumber = 0;
+    bool countingDown;
 
     Transform[] spawnPoints;
     Player localPlayer;              //the local player
     ExitGames.Client.Photon.Hashtable currentPlayerProperties = new ExitGames.Client.Photon.Hashtable();
 
-    bool allPlayersLoaded;      //true if all players are loaded into the scene
-    bool playersSpawning;       //true if players are loaded, starts setting player spawnpoints
-    bool playerSpawned;         //true if all players have spawned into the game
+
+    
+
+    #region Variables for player spawning
+    bool[] isSpawnPointTaken;
+    int numSpawnPointsTaken = 0;
+    List<Player> loadedPlayerList = new List<Player>();
+    #endregion
+
+    public event Action<Player> PlayerLoaded;
+    public event Action SpawnPlayers;
+
     private void Start()
     {
-        countdownText.text = "";
-        playersSpawning = false;
-        playerSpawned = false;
+        countdownText.Add("Duck");
+        countdownText.Add("Duck");
+        countdownText.Add("GOOSE!");
+        countdownAnimator = countdownTextBox.gameObject.GetComponent<Animator>();
+
+
+        PlayerLoaded += CheckIfAllPlayersLoaded;
+        SpawnPlayers += SpawnSelf;
+        countdownTextBox.gameObject.SetActive(false);
+        countingDown = false;
         if (PhotonNetwork.IsConnected)
         {
             localPlayer = PhotonNetwork.LocalPlayer;
             spawnPoints = GetComponentsInChildren<Transform>();
+            isSpawnPointTaken = new bool[spawnPoints.Length];
 
             currentPlayerProperties["HasLoadedStage"] = true;
             localPlayer.SetCustomProperties(currentPlayerProperties);
+            PlayerLoaded(localPlayer);
 
-        }
-    }
-
-    private void Update()
-    {
-        if (!playerSpawned)
-        {
-            SpawnPlayers();
-            return;
-        }
-        
-    }
-
-    private void SpawnPlayers()
-    {
-        if (localPlayer.CustomProperties.ContainsKey("shouldSpawn") && !playerSpawned)
-        {
-            spawnSelf();
-            playerSpawned = true;
-        }
-        if (!PhotonNetwork.IsMasterClient)
-        {
-            return;
-        }
-        if (!allPlayersLoaded)
-        {
-            allPlayersLoaded = true;
-            foreach (KeyValuePair<int, Player> player in PhotonNetwork.CurrentRoom.Players)
-            {
-                Player currentPlayer = player.Value;
-                if (!currentPlayer.CustomProperties.ContainsKey("HasLoadedStage"))
-                {
-                    allPlayersLoaded = false;
-                }
-            }
         }
         else
         {
-            if (!playersSpawning)
+            StartCountdown(); //test script
+        }
+    }
+
+    public event Action IntroCountdownEnded;
+    private void FixedUpdate()
+    {
+        if (countingDown)
+        {
+            if (countdownTimer.getWatch() > countdownNextNumber)
             {
-                playersSpawning = true;
-                SetPlayerSpawnpoints();
+                if (countdownNextNumber == countdownText.Count)
+                {
+                    countdownTextBox.gameObject.SetActive(false);
+                    countingDown = false;
+                }
+                else if(countdownNextNumber + 1 == countdownText.Count)
+                {
+                    countdownNextNumber++;
+                    countdownTextBox.text = countdownText[countdownNextNumber - 1];
+                    Debug.Log(countdownTextBox.text);
+                    countdownAnimator.SetTrigger("CountFinal");
+                }
+                else
+                {
+                    countdownNextNumber++;
+                    countdownTextBox.text = countdownText[countdownNextNumber - 1];
+                    countdownAnimator.SetTrigger("Count");
+                }
             }
         }
     }
 
-
-    private void SetPlayerSpawnpoints()
+    private void CheckIfAllPlayersLoaded(Player player)
     {
-        bool[] isSpawnPointTaken = new bool[spawnPoints.Length];
-        int numSpawnPointsTaken = 0;
-
-        foreach (KeyValuePair<int, Player> player in PhotonNetwork.CurrentRoom.Players)
+        if (PhotonNetwork.IsMasterClient)
         {
-            Player currentPlayer = player.Value;
-            currentPlayerProperties = currentPlayer.CustomProperties;
+            loadedPlayerList.Add(player);
+            
+            currentPlayerProperties = player.CustomProperties;
 
-            if (!currentPlayer.CustomProperties.ContainsKey("CurrentSpawnPoint"))
+            if (!player.CustomProperties.ContainsKey("CurrentSpawnPoint"))
             {
-                if(numSpawnPointsTaken < spawnPoints.Length - 1)
+                if (numSpawnPointsTaken < spawnPoints.Length - 1)
                 {
                     int spawnPointRand = Random.Range(0, spawnPoints.Length);
                     while (isSpawnPointTaken[spawnPointRand])
@@ -106,7 +118,7 @@ public class PlayerSpawner : MonoBehaviour
                 }
                 else
                 {
-                    for(int i = 0; i < spawnPoints.Length; i++)
+                    for (int i = 0; i < spawnPoints.Length; i++)
                     {
                         if (!isSpawnPointTaken[i])
                         {
@@ -114,24 +126,32 @@ public class PlayerSpawner : MonoBehaviour
                             break;
                         }
                     }
-                } 
+                }
             }
-            currentPlayer.SetCustomProperties(currentPlayerProperties);
-        }
+            player.SetCustomProperties(currentPlayerProperties);
 
-        foreach (KeyValuePair<int, Player> player in PhotonNetwork.CurrentRoom.Players)
-        {
-            Player currentPlayer = player.Value;
-            currentPlayerProperties = currentPlayer.CustomProperties;
-            currentPlayerProperties["shouldSpawn"] = true;
-            currentPlayer.SetCustomProperties(currentPlayerProperties);
+            if (loadedPlayerList.Count == PhotonNetwork.CurrentRoom.PlayerCount)
+            {
+                SpawnPlayers();
+            }
         }
     }
 
-    public void spawnSelf()
+    private void SpawnSelf()
     {
         string chosenChar = chars.getList[(int)PhotonNetwork.LocalPlayer.CustomProperties["SelectedChar"]].name;
         Vector3 spawnPoint = spawnPoints[(int)PhotonNetwork.LocalPlayer.CustomProperties["CurrentSpawnPoint"]].position;
         PhotonNetwork.Instantiate("DuckPrefabs/" + chosenChar, spawnPoint, Quaternion.identity);
+        StartCountdown();
+    }
+
+    public void StartCountdown()
+    {
+        countingDown = true;
+        countdownTextBox.text = countdownText[0];
+        countdownNextNumber = 1;
+        countdownTextBox.gameObject.SetActive(true);
+        countdownTimer.startWatch("countdown");
+        countdownAnimator.SetTrigger("Count");
     }
 }
